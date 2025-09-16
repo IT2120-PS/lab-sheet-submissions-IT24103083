b92dedac2616d66842263fb2672b0cf3c1222d0f
IT24103083 <it24103083@my.sliit.lk>
2025-09-16 19:53:03 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103083.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\B E S T  COMPUTER\\Desktop\\IT24103083")
+getwd()
+#Exercise 
+#Question 1
+punif(25, min=0, max=40) - punif(10,min=0, max=40)
+
+#Question 2
+pexp(2, rate=1/3)
+
+#Question 3
+#i
+1 - pnorm(130, mean=100, sd=15)
+
+#ii
+qnorm(0.95, mean=100, sd=15)
